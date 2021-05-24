eb50b49257c5cab5a33fea7cdf43b23a0be4de93
Dennis Follensbee <da.follensbee@gmail.com>
2021-05-24 08:59:40 -0400

    Update parallel_query_example.sql

--- parallel_query_example.sql
+++ parallel_query_example.sql
@@ -1,9 +1,6 @@
 SET max_parallel_workers = 6;
 SET max_parallel_workers_per_gather = 6;
 SET parallel_leader_participation = off;
-SET parallel_tuple_cost = 0;
-SET parallel_setup_cost = 0;
-SET min_parallel_table_scan_size = 0;
 
 CREATE TABLE risk.fixy AS
 SELECT *, ST_GeometryType(ST_CollectionExtract(ST_MakeValid(ST_SnapToGrid(geom, .000001)), 3))
